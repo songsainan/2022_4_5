18aa98107daa16a37349e3f23b65e91ddd31902c
songsainan <27000599@qq.com>
2022-05-26 22:10:11 +0800

    Default Changelist

--- common/setup_fun.py
+++ common/setup_fun.py
@@ -1,11 +1,17 @@
 import requests
 
 
+from common.handle_conf import confger
+
+
 class SetupFun:
     @classmethod
     def user_login(cls):
-        print('nihao')
-
-
-SetupFun.user_login()
+        url = 'http://api.lemonban.com/futureloan/member/login'
+        head = confger.get('evn', 'head')
+        body = {'mobile_phone': confger.get('evn', 'mobile_phone'),
+                'pwd': confger.get('evn', 'pwd')}
+        response = requests.request(method='post', url=url, headers=head, json=body)
+        res = response.json()
+        return res
 
